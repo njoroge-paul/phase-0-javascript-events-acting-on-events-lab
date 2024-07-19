f681e2b11e7e7e790d87b606cfaa499e116b94fa
Duncan <njorogedun870@gmail.com>
2024-07-19 14:15:45 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+// helpers.js
+let dodgerLeft = 0;
+
+function moveDodgerLeft() {
+  if (dodgerLeft > 0) {
+    dodgerLeft -= 10;
+  }
+  document.getElementById('dodger').style.left = `${dodgerLeft}px`;
+}
+
+function moveDodgerRight() {
+  if (dodgerLeft < 360) {
+    dodgerLeft += 10;
+  }
+  document.getElementById('dodger').style.left = `${dodgerLeft}px`;
+}
